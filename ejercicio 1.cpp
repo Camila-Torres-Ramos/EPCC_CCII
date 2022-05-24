2ac0fdedafea834d96fb928886ceaf57d6ddcaa6
Camila-Torres-Ramos <82998251+Camila-Torres-Ramos@users.noreply.github.com>
2022-05-23 19:29:19 -0500

    Add files via upload

---
+++ ejercicio 1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+ 
+int main() 
+{
+    int numero;
+ 
+    cout << "Escribe un numero: ";
+    cin >> numero;
+    if (numero>0)  cout << "El numero es positivo.";
+ 
+    return 0;
+}
